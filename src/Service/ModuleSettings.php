a4b4db32559babc4b757436cf9fc7d904dd11955
KiryiMONZTA <dev@kiryi.net>
2023-01-12 16:05:24 +0000

    Added module setting service.

---
+++ src/Service/ModuleSettings.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidAcademy\ProductPromotionBanner\Service;
+
+use OxidAcademy\ProductPromotionBanner\Core\Module;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Facade\ModuleSettingServiceInterface;
+use Symfony\Component\String\UnicodeString;
+
+/**
+ * @extendable-class
+ */
+class ModuleSettings
+{
+    public function __construct(private ModuleSettingServiceInterface $moduleSettingService) {}
+
+    public function getItemNumber(): int
+    {
+        return (int) $this->moduleSettingService->getInteger(Module::SETTING_ITEM_NUMBER, Module::MODULE_ID);
+    }
+
+    public function getDisplayPrice(): bool
+    {
+        return (bool) $this->moduleSettingService->getBoolean(Module::SETTING_DISPLAY_PRICE, Module::MODULE_ID);
+    }
+}
