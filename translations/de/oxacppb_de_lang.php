aacbef08cda0765f7e04400f78f8e793c51e3c25
KiryiMONZTA <dev@kiryi.net>
2024-05-13 10:19:02 +0000

    Removed setting to display price.

--- translations/de/oxacppb_de_lang.php
+++ translations/de/oxacppb_de_lang.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 $aLang = [
     'charset'                              => 'UTF-8',
-    'OXACPPB_PROMOTION_MESSAGE'            => 'Kaufe jetzt das %s in unserem Shop!',
-    'OXACPPB_PROMOTION_MESSAGE_WITH_PRICE' => 'Kaufe jetzt das %s für nur %s in unserem Shop!',
+    'OXACPPB_PROMOTION_MESSAGE'            => 'Kaufe jetzt das Produkt %s für nur %s in unserem Shop!',
     'OXACPPB_BUTTON_TEXT'                  => 'Jetzt zuschlagen',
 ];
--- translations/en/oxacppb_en_lang.php
+++ translations/en/oxacppb_en_lang.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 $aLang = [
     'charset'                              => 'UTF-8',
-    'OXACPPB_PROMOTION_MESSAGE'            => 'Buy now the %s in our Shop!',
-    'OXACPPB_PROMOTION_MESSAGE_WITH_PRICE' => 'Buy now the %s for just %s in our Shop!',
+    'OXACPPB_PROMOTION_MESSAGE'            => 'Buy now the product %s for just %s in our Shop!',
     'OXACPPB_BUTTON_TEXT'                  => 'Get it now',
 ];
