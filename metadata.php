001a73e8ab410923a70d6f3efbb542a0b84912e9
KiryiMONZTA <dev@kiryi.net>
2024-07-05 15:36:10 +0000

    Removed whitespaces and license info.

--- metadata.php
+++ metadata.php
@@ -1,11 +1,6 @@
 <?php
 
 /**
- * Copyright © OXID eSales AG. All rights reserved.
- * See LICENSE file for license details.
- */
-
-/**
  * Metadata version
  */
 $sMetadataVersion = '2.1';
@@ -14,18 +9,18 @@ $sMetadataVersion = '2.1';
  * Module information
  */
 $aModule = [
-    'id'          => 'oxacppb',
-    'title'       => 'OXID Academy Product Promotion Banner',
+    'id' => 'oxacppb',
+    'title' => 'OXID Academy Product Promotion Banner',
     'description' => [
         'en' => 'Shows promotion banner on start page for a selected product.',
         'de' => 'Zeigt einen Werbebanner für ein ausgewähltes Produkt auf der Startseite an.',
     ],
-    'thumbnail'   => 'pictures/logo.svg',
-    'version'     => '2.0.0',
-    'author'      => 'OXID Academy',
-    'url'         => 'https://www.oxid-esales.com/ressourcen/academy/',
-    'email'       => 'academy@oxid-esales.com',
-    'extend'      => [
+    'thumbnail' => 'pictures/logo.svg',
+    'version' => '2.0.0',
+    'author' => 'OXID Academy',
+    'url' => 'https://www.oxid-esales.com/ressourcen/academy/',
+    'email' => 'academy@oxid-esales.com',
+    'extend' => [
         OxidEsales\Eshop\Application\Controller\StartController::class => OxidAcademy\ProductPromotionBanner\Controller\StartController::class,
     ],
     'settings' => [
